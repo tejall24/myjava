b25251c39f7d4908f86d008ee93da1d1ea5056c3
tejal <1002tejalmogal@gmail.com>
2025-04-25 07:21:35 +0000

    commit2

--- myfile.java
+++ myfile.java
@@ -1,7 +1,5 @@
-public class myfile
-{
-    public static void main(String[] args)
-    {
-        System.out.println(“Hello, World”);
+public class myfile {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
     }
 }
